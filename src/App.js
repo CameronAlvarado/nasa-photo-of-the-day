87779e4058e5eaaf8e32a3f83828c4846d41a465
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-14 13:53:15 -0700

    structured site, working on axios and map function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
+import Header from "./components/Header"
+import Body from "./components/Body"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        app! Have fun!
+        <Header />
+        <Body />
+        <Footer />
       </p>
     </div>
   );
 }
 
 export default App;
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
